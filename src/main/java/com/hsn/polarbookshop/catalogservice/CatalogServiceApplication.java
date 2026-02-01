a6821f7615a1046c924c7f23f86b9c352b2d76ac
mubarak <Skinnynigg334@gmail.com>
2026-02-01 21:41:27 +0000

    added the unversioned files

---
+++ src/main/java/com/hsn/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hsn.polarbookshop.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+    }
+
+}
